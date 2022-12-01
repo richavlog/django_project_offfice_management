0d3ac9c5d85b6c5f61d90cca45ea39c49e87c07c
richavlog <105155803+richavlog@users.noreply.github.com>
2022-12-01 18:08:59 +0530

    Add files via upload

---
+++ office employees management system/office_emp_proj/emp_app/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Employee,Role,Department 
+# Register your models here.
+admin.site.register(Employee)
+admin.site.register(Role)
+admin.site.register(Department)
+
+
+
+
+
+
+
+#superuser name=admin,email=richa@gmail.com,pw=123456789
